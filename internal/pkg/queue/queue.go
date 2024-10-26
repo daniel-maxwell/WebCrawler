994ddbb59fb8b4b25f3bb7a5fa960ef365297308
Daniel White <dm.white@gmx.com>
2024-10-27 00:01:36 +0100

    Added comments to queue methods

--- internal/pkg/queue/queue.go
+++ internal/pkg/queue/queue.go
@@ -9,7 +9,8 @@ type Queue struct {
     Elements []int 
     Size     int 
 } 
-  
+
+// Creates a new queue
 func (q *Queue) Enqueue(elem string) { 
     if q.GetLength() == q.Size { 
         fmt.Println("Overflow") 
@@ -17,7 +18,8 @@ func (q *Queue) Enqueue(elem string) {
     } 
     q.Elements = append(q.Elements, elem) 
 } 
-  
+
+// Removes the first element of the queue
 func (q *Queue) Dequeue() string { 
     if q.IsEmpty() { 
         fmt.Println("UnderFlow") 
@@ -31,15 +33,18 @@ func (q *Queue) Dequeue() string {
     q.Elements = q.Elements[1:] 
     return element // Slice off the element once it is dequeued. 
 } 
-  
+
+// Returns the length of the queue
 func (q *Queue) GetLength() string { 
     return len(q.Elements) 
 } 
-  
+
+// Returns true if the queue is empty, false otherwise
 func (q *Queue) IsEmpty() bool { 
     return len(q.Elements) == 0
 } 
-  
+
+// Returns the first element of the queue
 func (q *Queue) Peek() (int, error) { 
     if q.IsEmpty() { 
         return "", errors.New("empty queue") 
